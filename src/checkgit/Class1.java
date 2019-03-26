bd355962fb56727ce712215bf4045a707fffad42
xlco1 <cloautomation@gmail.com>
2019-03-26 13:54:09 -0700

    first commit -saran

---
+++ src/checkgit/Class1.java
@@ -0,0 +1,18 @@
+package checkgit;
+
+
+
+public class Class1 {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("first");
+		/*
+		 * 
+		 * code
+		 * 
+		 */
+		System.out.println("second");		
+	}
+
+}
